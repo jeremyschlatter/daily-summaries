d0ad60f6965fb112bd39391b44e4d27ced30322d
JeremySchlatter <jeremy.schlatter@gmail.com>
2014-01-07 19:59:08 -0500

    switch to always loading content scripts rather than programmatically injecting (just for development), browser action brings up prompt on all tabs

--- background.js
+++ background.js
@@ -3,10 +3,9 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     alert("go to the options page and set the spreadsheet id");
     return;
   }
-  chrome.tabs.insertCSS(null, {file: "smoke.css"});
-  chrome.tabs.executeScript(null, {file: "smoke.min.js"});
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, localStorage["ss_id"]);
+  chrome.tabs.query({}, function(tabs) {
+    for (var i=0; i<tabs.length; ++i) {
+      chrome.tabs.sendMessage(tabs[i].id, localStorage["ss_id"]);
+    }
   });
 });
--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
 function doit(key, worksheet, text) {
-  text = text.replace(/&/g, '&amp;')
+  var text = text.replace(/&/g, '&amp;')
              .replace(/</g, '&lt;')
              .replace(/>/g, '&gt;')
              .replace(/"/g, '&quot;')
@@ -10,12 +10,14 @@ function doit(key, worksheet, text) {
   req.send('<entry xmlns="http://www.w3.org/2005/Atom" xmlns:gsx="http://schemas.google.com/spreadsheets/2006/extended"><gsx:thisisthesummarycolumn>' + text + '</gsx:thisisthesummarycolumn></entry>');
 }
 
-chrome.runtime.onMessage.addListener(
-  function(spreadsheetKey, sender, sendResponse) {
-    smoke.prompt("What did you do today?", function(userText){
-      if (userText){
-        doit(spreadsheetKey, "od6", userText);
-      }else{
-      }
-    }, {ok: "Submit"});
-  });
+prompter = function(spreadsheetKey, sender, sendResponse) {
+  smoke.prompt("What did you do today?", function(userText){
+    if (userText){
+      //doit(spreadsheetKey, "od6", userText);
+    }else{
+    }
+    //chrome.runtime.onMessage.removeListener(prompter);
+  }, {ok: "Submit"});
+};
+
+chrome.runtime.onMessage.addListener(prompter);
