4fca93ce4f32cc1b62c444dada029e1046b3aeb8
JeremySchlatter <jeremy.schlatter@gmail.com>
2014-01-07 19:21:40 -0500

    browser action triggers prompt, text is recorded to spreadsheet given in options page

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+  if (!localStorage["ss_id"]) {
+    alert("go to the options page and set the spreadsheet id");
+    return;
+  }
   chrome.tabs.insertCSS(null, {file: "smoke.css"});
   chrome.tabs.executeScript(null, {file: "smoke.min.js"});
   chrome.tabs.executeScript(null, {file: "content_script.js"});
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, localStorage["ss_id"]);
+  });
 });
--- content_script.js
+++ content_script.js
@@ -10,9 +10,12 @@ function doit(key, worksheet, text) {
   req.send('<entry xmlns="http://www.w3.org/2005/Atom" xmlns:gsx="http://schemas.google.com/spreadsheets/2006/extended"><gsx:thisisthesummarycolumn>' + text + '</gsx:thisisthesummarycolumn></entry>');
 }
 
-smoke.prompt("What did you do today?", function(e){
-  if (e){
-    doit("0AhB1LVNb2qc8dEhmSzJmNTRLVVZ0XzZwdHQyX3dKbmc", "od6", e);
-  }else{
-  }
-}, {ok: "Submit"});
+chrome.runtime.onMessage.addListener(
+  function(spreadsheetKey, sender, sendResponse) {
+    smoke.prompt("What did you do today?", function(userText){
+      if (userText){
+        doit(spreadsheetKey, "od6", userText);
+      }else{
+      }
+    }, {ok: "Submit"});
+  });
